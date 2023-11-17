e9e91618af35c12b9fe78e3c277deb397d768fc2
Minsu Kim <minsu_kim@bishanoi.net>
2023-11-17 23:06:16 +0700

    :fire: chore: Delete .wrangler directory

--- .wrangler/tmp/bundle-69UM10/middleware-insertion-facade.js
+++
@@ -1,3 +0,0 @@
-import { __facade_registerInternal__ } from "/workspaces/discord-mailbox/node_modules/wrangler/templates/middleware/loader-sw.ts";
-import * as __MIDDLEWARE_0__ from "/workspaces/discord-mailbox/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
-__facade_registerInternal__([__MIDDLEWARE_0__.default])
